b55be179ca2bc48e98d866b6c8948ca4a3b640d3
Lucas Wagner <lowagner@gmail.com>
2024-01-15 13:11:29 -0800

    add GPU Texture to Pixels

--- src/gpu.rs
+++ src/gpu.rs
@@ -1,13 +1,40 @@
 #![allow(dead_code)]
 
-pub struct Gpu {}
+use crate::dimensions::*;
+use crate::pixels::*;
+
+pub struct Gpu {
+    device: wgpu::Device,
+}
 
 impl Gpu {
+    pub fn pixels(&mut self, size: Size2i) -> Pixels {
+        let description = wgpu::TextureDescriptor {
+            size: wgpu::Extent3d {
+                width: size.width() as u32,
+                height: size.height() as u32,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8Unorm,
+            usage: wgpu::TextureUsages::all(),
+            label: None,
+            view_formats: &[],
+        };
+        let texture = self.device.create_texture(&description);
+        Pixels {
+            size,
+            synced: Synced::GpuOnly,
+            array: vec![],
+            texture: Some(texture),
+        }
+    }
+
     // TODO: draw(from: &mut Pixels, from_rectangle: RectangleI, to: &mut Pixels, to_rectangle: RectangleI)
     // TODO: draw(pixels: &mut Pixels, coordinates: CoordinatesI, color: Color)
     // TODO: ensure(pixels: &mut Pixels, on: On)
-
-    // TODO: `pixels(size: Size2i) -> Pixels` -> creating pixels on the GPU only
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
--- src/pixels.rs
+++ src/pixels.rs
@@ -5,7 +5,7 @@ use crate::dimensions::*;
 use crate::gpu::Synced;
 
 pub struct Pixels {
-    size: Size2i,
+    pub(crate) size: Size2i,
     pub(crate) synced: Synced,
     /// Rows of pixels from the top (y = 0) to the bottom (y = self.size.height() - 1),
     /// with each row going from left (x = 0) to right (x = self.size.width() - 1).
@@ -13,6 +13,8 @@ pub struct Pixels {
     /// Invariant: each `Vec` has the correct size based on `self.size`,
     /// unless the pixels are stored only on the GPU.
     pub(crate) array: Vec<Vec<Color>>,
+    /// Invariant: this should be `Some(texture)` iff `self.synced.on_gpu()` is true.
+    pub(crate) texture: Option<wgpu::Texture>,
 }
 
 impl Pixels {
@@ -21,6 +23,7 @@ impl Pixels {
             size,
             synced: Synced::CpuOnly,
             array: Self::transparent_pixels_array(size),
+            texture: None,
         }
     }
 
