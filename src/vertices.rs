8543758bd82190afaf75e1c64a99f790d592c077
Lucas Wagner <lowagner@gmail.com>
2024-01-21 22:14:51 -0800

    allow moving Vertices to a GPU buffer

--- src/vertices.rs
+++ src/vertices.rs
@@ -1,9 +1,29 @@
 #![allow(dead_code)]
 
-use crate::variables::*;
+use crate::gpu::*;
+
+use wgpu::util::DeviceExt; // create_buffer_init
 
 pub struct Vertices<V: Variables> {
     /// The `Variables` in `V` must be settable, because we are specifying
     /// the values of each vertex (e.g., position, color, etc.).
     array: Vec<V>,
+    /// Present if the vertices are on the GPU.
+    buffer: Option<wgpu::Buffer>,
+}
+
+impl<V: Variables + bytemuck::Pod> Vertices<V> {
+    pub(crate) fn ensure_on_gpu(&mut self, gpu: &mut Gpu) {
+        if self.buffer.is_some() {
+            return;
+        }
+        self.buffer = Some(
+            gpu.device
+                .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: None, // TODO: consider adding `label` to Vertices struct and adding it here.
+                    contents: bytemuck::cast_slice(&self.array[..]),
+                    usage: wgpu::BufferUsages::VERTEX,
+                }),
+        );
+    }
 }
